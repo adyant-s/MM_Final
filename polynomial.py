dc171fbbe01570cb90845cc7d60a5ee7be645535
adyant-s <54739903+adyant-s@users.noreply.github.com>
2023-10-20 06:24:36 +0530

    Create polynomial.py

---
+++ polynomial.py
@@ -0,0 +1,15 @@
+import numpy
+from sklearn.metrics import r2_score
+
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+
+mymodel = numpy.poly1d(numpy.polyfit(x, y, 3))
+yx = []
+xx=[]
+for i in range(0, len(y)+10):
+  xx.append(i+1)
+  yx.append(mymodel(i))
+
+plt.plot(x, y, c='r')
+plt.plot(xx, yx, c='g')
